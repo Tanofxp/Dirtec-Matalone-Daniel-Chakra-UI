6fca110a70dbe59dbe5037cb08a5e8935d07ceb9
Daniel Matalone <matalone.daniel@moron.gob.ar>
2022-07-03 17:03:03 -0300

    text cart + favicon

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,7 +1,22 @@
+import { 
+    Box,
+    Heading,
+    Text,
+    Center
+        } from '@chakra-ui/react'
 import React from 'react'
 
 export default function Cart() {
-  return (
-    <div>este es el cart, hola cómo estás</div>
-  )
+    return (
+
+    <Box alignContent='center' margin={'auto'} mt={5} h={'auto'} padding={5} w={1000} border={'solid'} rounded={'lg'} >
+        <Text></Text>
+        <Center h='100px'>
+        <Heading>Finalizaste Tu Compra</Heading>
+        </Center>
+        <Center h='100px'>
+        <Text>¿No sabés qué comprar? ¡Miles de productos te esperan!</Text>
+        </Center>
+    </Box>
+    )
 }
